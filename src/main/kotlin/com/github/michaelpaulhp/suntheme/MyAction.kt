231ec3b8069290534f4e260c44e7596263a85cec
GitHub Action <action@github.com>
2023-07-30 19:42:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/michaelpaulhp/suntheme/MyAction.kt
+++ src/main/kotlin/com/github/michaelpaulhp/suntheme/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.michaelpaulhp.suntheme
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/michaelpaulhp/suntheme/MyBundle.kt
+++ src/main/kotlin/com/github/michaelpaulhp/suntheme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.michaelpaulhp.suntheme
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
